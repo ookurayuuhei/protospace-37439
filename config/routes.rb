4513de44da38923538f94cf2984898424c86d0a8
ookurayuuhei <ookura74@gmail.com>
2022-02-24 23:37:46 +0900

    条件分岐

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'prototypes/index'
+  # get 'prototypes/index'
   root to: "prototypes#index"
+  resources :prototypes do
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
