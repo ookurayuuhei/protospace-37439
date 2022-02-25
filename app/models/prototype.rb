6e158f17d2b3a632ab07e7c2acb269c45d1df113
ookurayuuhei <ookura74@gmail.com>
2022-02-26 01:39:50 +0900

    投稿実装機能2

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,10 +2,10 @@ class Prototype < ApplicationRecord
 
   validates :title, presence: true
   validates :catch_copy, presence: true
-  validates :consept, presence: true
+  validates :concept, presence: true
   validates :image, presence: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :user
   has_one_attached :image
 end
