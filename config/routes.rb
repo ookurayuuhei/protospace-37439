a9b25eea4461a92885cf7f131e950549632bd0a4
ookurayuuhei <ookura74@gmail.com>
2022-02-25 21:39:38 +0900

    投稿機能実装1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'prototypes/index'
   root to: "prototypes#index"
+  resources :prototypes, only: [ :new, :create]
+
   resources :prototypes do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
