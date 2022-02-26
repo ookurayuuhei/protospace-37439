2549ce03c25526a91d86841717014b8dbbebebea
ookurayuuhei <ookura74@gmail.com>
2022-02-26 21:44:12 +0900

    プロトタイプ削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'prototypes/index'
   root to: "prototypes#index"
-  resources :prototypes, only: [ :new, :create, :show, :edit, :update, :destroy]
-  resources :prototypes do
-  end
+  resources :prototypes, only: [ :new, :create, :show, :edit, :update, :destroy ]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
