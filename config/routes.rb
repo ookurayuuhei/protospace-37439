9eafd66094414d0a5f0ed334f90d3329b24d5cd4
ookurayuuhei <ookura74@gmail.com>
2022-02-27 20:10:25 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'comment/create'
   devise_for :users
-  # get 'prototypes/index'
+  get 'prototypes/index'
   root to: "prototypes#index"
-  resources :prototypes , only: [ :new, :create, :show, :edit, :update, :destroy ] do
+  resources :prototypes do
   resources :comments, only: :create
 end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
 end
