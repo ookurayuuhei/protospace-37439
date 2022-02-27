6c61703dcce394605ca6f8ca739ba5955bcc361f
ookurayuuhei <ookura74@gmail.com>
2022-02-27 10:55:06 +0900

    comments controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get 'prototypes/index'
   root to: "prototypes#index"
   resources :prototypes, only: [ :new, :create, :show, :edit, :update, :destroy ]
+  resources :comments, only: :create
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
